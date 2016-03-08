536ea70c7be79d4b40c23580c9d052e265c7400e
David B Heise <david.heise@equifax.com>
2016-03-08 16:46:45 +0000

    removing partial classes

--- WinService.cs
+++ WinService.cs
@@ -11,10 +11,40 @@ namespace WinService
 	using System.ServiceProcess;
 	using System.Text;
 	using System.Threading;
-	partial class WinService : ServiceBase
+	class WinService : ServiceBase
 	{
 		private Worker worker;
 		private Thread workerThread;
+
+		#region Component Designer generated code
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			components = new System.ComponentModel.Container();
+			this.ServiceName = "WinService";
+		}
+
+		#endregion
 
 		public WinService()
 		{
@@ -46,7 +76,7 @@ namespace WinService
 			if (workerThread != null && workerThread.IsAlive)
 			{
 				worker.Stop();
-				workerThread.Abort();				
+				workerThread.Abort();
 			}
 		}
 
